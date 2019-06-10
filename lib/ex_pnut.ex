acab86802d4f54579927c05d5592355b152d6c02
Pascal Schmid <psschmid@gmx.de>
2019-06-10 18:43:12 +0200

    Initial commit

---
+++ lib/ex_pnut.ex
@@ -0,0 +1,18 @@
+defmodule ExPnut do
+  @moduledoc """
+  Documentation for ExPnut.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExPnut.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_pnut_test.exs
@@ -0,0 +1,8 @@
+defmodule ExPnutTest do
+  use ExUnit.Case
+  doctest ExPnut
+
+  test "greets the world" do
+    assert ExPnut.hello() == :world
+  end
+end
