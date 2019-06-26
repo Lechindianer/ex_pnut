ea1dc3eb656f91d281f06c12cd7a6f0c404371ab
Pascal Schmid <psschmid@gmx.de>
2019-06-26 08:36:39 +0200

    Add default headers helper

--- lib/helper/http.ex
+++ lib/helper/http.ex
@@ -2,11 +2,7 @@ defmodule ExPnut.Helper.HTTP do
   @moduledoc false
 
   def get(access_token, url) do
-    headers = [
-      {"X-Pretty-Json", "1"},
-      {"Authorization", "Bearer #{access_token}"},
-    ]
-
+    headers = default_headers(access_token)
     endpoint = Application.get_env(:ex_pnut, :endpoint)
 
     HTTPoison.get!("#{endpoint}#{url}", headers)
@@ -15,16 +11,19 @@ defmodule ExPnut.Helper.HTTP do
   end
 
   def post(access_token, url, payload) do
-    headers = [
-      {"Authorization", "Bearer #{access_token}"},
-      {"Content-Type", "application/json"},
-      {"X-Pretty-Json", "1"},
-    ]
-
+    headers = default_headers(access_token)
     endpoint = Application.get_env(:ex_pnut, :endpoint)
 
     HTTPoison.post!("#{endpoint}#{url}", payload, headers)
     |> ExPnut.Decode.decode
     |> Map.get(:data)
+  end
+
+  defp default_headers(access_token) do
+    [
+      {"Authorization", "Bearer #{access_token}"},
+      {"Content-Type", "application/json"},
+      {"X-Pretty-Json", "1"},
+    ]
   end
 end
