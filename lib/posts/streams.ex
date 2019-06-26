6577ebb97b82d23e101bdb1c386f6054ffd158a0
Pascal Schmid <psschmid@gmx.de>
2019-06-26 08:56:40 +0200

    Use HTTP helper for Streams

--- lib/posts/streams.ex
+++ lib/posts/streams.ex
@@ -1,40 +1,29 @@
 defmodule ExPnut.Posts.Streams do
+  import ExPnut.Helper.HTTP
+
   @moduledoc false
 
   def me(access_token) do
-    get_data(access_token, "/posts/streams/me")
+    get(access_token, "/posts/streams/me")
   end
 
   def unified(access_token) do
-    get_data(access_token, "/posts/streams/unified")
+    get(access_token, "/posts/streams/unified")
   end
 
   def mentions(access_token, user_id) do
-    get_data(access_token, "/users/#{user_id}/mentions")
+    get(access_token, "/users/#{user_id}/mentions")
   end
 
   def posts(access_token, user_id) do
-    get_data(access_token, "/users/#{user_id}/posts")
+    get(access_token, "/users/#{user_id}/posts")
   end
 
   def global(access_token) do
-    get_data(access_token, "#/posts/streams/global")
+    get(access_token, "#/posts/streams/global")
   end
 
   def tag(access_token, tag) do
-    get_data(access_token, "/posts/tag/#{tag}")
-  end
-
-  defp get_data(access_token, url) do
-    headers = [
-      {"X-Pretty-Json", "1"},
-      {"Authorization", "Bearer #{access_token}"},
-    ]
-
-    endpoint = Application.get_env(:ex_pnut, :endpoint)
-
-    HTTPoison.get!("#{endpoint}#{url}", headers)
-    |> ExPnut.Decode.decode
-    |> Map.get(:data)
+    get(access_token, "/posts/tag/#{tag}")
   end
 end
