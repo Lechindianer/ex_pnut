064836cd1d7603484f8c76e76c07b9e149cce8b9
Pascal Schmid <psschmid@gmx.de>
2019-06-29 09:52:43 +0200

    Add missing Lifecycle methods

--- lib/posts/lifecycle.ex
+++ lib/posts/lifecycle.ex
@@ -11,4 +11,15 @@ defmodule ExPnut.Posts.Lifecycle do
                     |> elem(1)
     post(client, "/posts", post_jsonified, post_params)
   end
+
+  def revise_post(client, postId, new_post = %NewPost{}, post_params = %PostParams{} \\ %PostParams{}) do
+    post_jsonified = new_post
+                     |> Jason.encode
+                     |> elem(1)
+    put(client, "/posts/#{postId}", post_jsonified, post_params)
+  end
+
+  def delete_post(client, postId) do
+    delete(client, "/posts/#{postId}")
+  end
 end
