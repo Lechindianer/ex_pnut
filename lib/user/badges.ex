b256d28e8bd087e267c083244c509d0af2981733
Pascal Schmid <psschmid@gmx.de>
2019-07-10 22:16:42 +0200

    Add user badges

---
+++ lib/user/badges.ex
@@ -0,0 +1,38 @@
+defmodule ExPnut.User.Badges do
+  import ExPnut.Helper.HTTP
+  alias ExPnut.User.UserParams
+
+  @moduledoc """
+  Badges are accessible to users who are current badge holders.
+
+  A user can set what badge to display on their profile from their account, or a client can update it by updating the user.
+
+  Endpoints:
+  - Get a badge
+  - Get all badges
+  - Get the authenticated user's badges
+
+  [https://pnut.io/docs/api/resources/users/badges](https://pnut.io/docs/api/resources/users/badges)
+  """
+
+  @doc """
+  Retrieve a badge object.
+  """
+  def get_badge(client, badge_id, %UserParams{} = user_params \\ %UserParams{}) do
+    get(client, "/badges/#{badge_id}", user_params)
+  end
+
+  @doc """
+  Retrieve a list of all badges. It is not paginated.
+  """
+  def get_badges(client, %UserParams{} = user_params \\ %UserParams{}) do
+    get(client, "/badges", user_params)
+  end
+
+  @doc """
+  Retrieve a list of all badges for the authenticated user. It is not paginated.
+  """
+  def get_my_badges(client, %UserParams{} = user_params \\ %UserParams{}) do
+    get(client, "/users/me/badges", user_params)
+  end
+end
